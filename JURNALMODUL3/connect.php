30a529ffc3414e3d2958b93b4ef0d2865955c99e
MahesaYogha <116375160+MahesaYogha@users.noreply.github.com>
2023-11-13 22:53:17 +0700

    connect.php

--- JURNALMODUL3/connect.php
+++ JURNALMODUL3/connect.php
@@ -10,11 +10,11 @@ $password="";
 $database="modul3_wad";
 
 
-$connect= new mysqli($server,$username,$password,$database);
+$connection= new mysqli($server,$username,$password,$database);
 // 
   
 // Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
-if ($connect->connect_error) 
+if ($connection->connect_error) 
     
   
   echo "Koneksi Gagal ; [Keasalahan Koneksi]: ";
