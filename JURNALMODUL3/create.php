d92ca3223039d78d71fb03b8f346024ec6049e0d
MahesaYogha <116375160+MahesaYogha@users.noreply.github.com>
2023-11-13 22:53:52 +0700

    create.php

--- JURNALMODUL3/create.php
+++ JURNALMODUL3/create.php
@@ -8,40 +8,35 @@ include ("connect.php");
 
 // (2) Buatlah perkondisian untuk memeriksa apakah permintaan saat ini menggunakan metode POST
 
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+// 
 
 // (3) Jika sudah coba deh kalian ambil data dari form (CLUE : pakai POST)
 
     // a. Ambil data nama mobil
-        $nama_mobil=$_POST['nama_mobil'];
+    $nama_mobil = $_POST['nama_mobil'];
     // b. Ambil data brand mobil
-        $brand_mobil=$_POST['brand_mobil'];
+    $brand_mobil = $_POST['brand_mobil'];
     // c. Ambil data warna mobil
-        $warna_mobil=$_POST['warna_mobil'];
+    $warna_mobil = $_POST['warna_mobil'];
     // d. Ambil data tipe mobil
-        $tipe_mobil=$_POST['tipe_mobil'];   
+    $tipe_mobil = $_POST['tipe_mobil'];
     // e. Ambil data harga mobil
-        $harga_mobil=$_POST['harga_mobil'];
+    $harga_mobil = $_POST['harga_mobil'];
+}
     // (4) Kalau sudah, kita lanjut Query / Menambahkan data pada SQL (Disini ada perintah untuk SQL), Masukkan ke tabel showroom_mobil (include setiap nama column)
 
-    $query = mysqli_query ($connect, "INSERT INTO showroom_mobil(nama_mobil, brand_mobil, warna_mobil, tipe_mobil, harga_mobil) 
-                                VALUES ('$nama_mobil', '$brand_mobil', '$warna_mobil', '$tipe_mobil', '$harga_mobil') ");
-
+$query = mysqli_query ($connection, "INSERT INTO showroom_mobil(nama_mobil, brand_mobil, warna_mobil, tipe_mobil, harga_mobil) VALUES ('$nama_mobil', '$brand_mobil', '$warna_mobil', '$tipe_mobil', '$harga_mobil')");
     // (5) Buatkan kondisi jika eksekusi query berhasil
-    if($query) {
-        echo "<script>alert('Berhasil ditambahkan')</script>";
-        echo "<meta http-equiv='refresh' content='1' url='form_create_mobil.php'>";
-    }else {
-        echo "<script>alert('Gagal ditambahkan')</script>";
-        echo "<meta http-equiv='refresh' content='1' url='form_create_mobil.php'>";
-    }
-
-
+if ($query) {
+    echo "<script>alert('Data Berhasil Disimpan')</script>";
+    echo "<meta http-equiv='refresh' content='1 url=index.php'>";
+}
     // (6) Jika terdapat kesalahan, buatkan eksekusi query gagalnya 
-
+else {
+    echo "<script>alert('Error')</script>";
+    echo "<meta http-equiv='refresh' content='1 url=index.php'>";
+}
 // (7) Tutup koneksi ke database setelah selesai menggunakan database
-
-$connect->close();
+$connection->close();
 ?>
